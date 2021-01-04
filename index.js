b0e897634d138bc3fe9e396bf43f374440e45a2b
Bellumxyz <76966530+Bellumxyz@users.noreply.github.com>
2021-01-04 19:27:14 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js'),
+    client = new Discord.Client({
+        fetchAllMembers: true
+    }),
+    config = require('./config.json'),
+    fs = require('fs')
+ 
+client.login(process.env.TOKEN)
